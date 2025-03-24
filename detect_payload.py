d656f2cf930da6cc5f166c072a68bb865aab2abe
WyldWest <coleb3000@gmail.com>
2025-03-24 15:24:19 -0600

    Update detect_payload.py

--- detect_payload.py
+++ detect_payload.py
@@ -4,17 +4,14 @@ from time import sleep
 
 # Reed swich on pin 0 using internal pull down resistor, other wire of switch connects to 3.3V
 reed_switch = Pin(16, Pin.IN, Pin.PULL_DOWN)
-led = Pin('LED', Pin.OUT)
+led = Pin(3, Pin.OUT) #make sure this works
 
 while True:
     if reed_switch.value() == 1:  # Check if the magnet is near
-        led.value(1)# Turn on the LED
-        sleep(0.5)
-        led.value(0)# Turn off to blink
-        sleep (0.2)
-        
+        led.value(0)# Turn on the LED
+    
     else:
-        led.value(0)  # Turn off the LED
+        led.value(1)  # Turn off the LED
        
     
     print(reed_switch.value())
